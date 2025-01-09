e19755dc3304ebd84b43dbdfd14578d018fcff3a
Abdulloh <R.abdulloh04@icloud.com>
2025-01-09 10:14:58 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,216 @@
+<template>
+  <main>
+    <section class="relative">
+      <div class="container">
+        <div
+          class="text-white flex flex-col items-start max-w-[600px] w-full pt-[200px] pb-[277px] relative z-10"
+        >
+          <span class="mb-8 text-base text-primary uppercase"
+            >Architecture design
+          </span>
+          <h1 class="mb-3 text-7xl font-normal uppercase">
+            Professional Interior Design
+          </h1>
+          <p class="mb-12 text-gray text-xl font-normal">
+            Design for now with the awareness of yesterday and unknown tomorrow.
+            Because small details make the perfect design. Helping you choose
+            between good and best.
+          </p>
+          <a
+            href=""
+            class="uppercase font-medium py-4 px-8 border text-white text-base"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+      <span
+        class="absolute bottom-0 left-0 text-dark/40 font-extrabold text-9xl uppercase"
+      >
+        Interior
+      </span>
+      <img src="/images/path.svg" alt="Path" class="absolute top-0 left-0" />
+      <img
+        class="absolute top-0 right-0 w-1/2 h-full object-cover"
+        src="/images/hero-img.png"
+        alt="Hero background"
+      />
+    </section>
+    <section class="py-[140px]">
+      <div class="container">
+        <h3
+          class="text-center mb-3 uppercase text-primary text-base font-normal"
+        >
+          top services
+        </h3>
+        <h2 class="capitalize text-center text-white font-normal text-6xl">
+          Our Specializations
+        </h2>
+        <div class="mt-12 grid grid-cols-4 gap-8">
+          <div class="relative -top-[130px] overflow-hidden rounded-2xl">
+            <img
+              class="w-full object-cover"
+              src="/images/image1.png"
+              alt="Image"
+            />
+            <div
+              class="p-8 absolute bottom-0 left-0 bg-gradient-to-t from-[#10131C]"
+            >
+              <h4 class="text-white font-normal mb-2 text-2xl">Floor plan</h4>
+              <p class="text-white text-base font-normal">
+                Tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div class="relative overflow-hidden rounded-2xl">
+            <img
+              class="w-full object-cover"
+              src="/images/image2.png"
+              alt="Image"
+            />
+            <div
+              class="p-8 absolute bottom-0 left-0 bg-gradient-to-t from-[#10131C]"
+            >
+              <h4 class="text-white font-normal mb-2 text-2xl">
+                Interior Design
+              </h4>
+              <p class="text-white text-base font-normal">
+                Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+              </p>
+            </div>
+          </div>
+          <div class="relative overflow-hidden rounded-2xl">
+            <img
+              class="w-full object-cover"
+              src="/images/image3.png"
+              alt="Image"
+            />
+            <div
+              class="p-8 absolute bottom-0 left-0 bg-gradient-to-t from-[#10131C]"
+            >
+              <h4 class="text-white font-normal mb-2 text-2xl">
+                Architecture Design
+              </h4>
+              <p class="text-white text-base font-normal">
+                Tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+          <div class="relative -top-[130px] overflow-hidden rounded-2xl">
+            <img
+              class="w-full object-cover"
+              src="/images/image4.png"
+              alt="Image"
+            />
+            <div
+              class="p-8 absolute bottom-0 left-0 bg-gradient-to-t from-[#10131C]"
+            >
+              <h4 class="text-white font-normal mb-2 text-2xl">Construction</h4>
+              <p class="text-white text-base font-normal">
+                Tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <span
+        class="absolute bottom-0 right-0 text-dark/40 font-extrabold text-9xl uppercase"
+      >
+        Interior
+      </span>
+    </section>
+    <section>
+      <div class="container flex">
+        <div class=" text-white py-16 px-6">
+          <div class="mx-auto">
+            <h2 class="text-4xl font-bold mb-6">About Us</h2>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 items-center">
+              <div class="lg:col-span-1">
+                <div class="flex gap-[30px] items-center px-[15px]">
+                  <img src="../public/images/Rectangle 760.png" alt="">
+                  <p class="max-w-[940px] w-full font-normal text-[16px] leading-[26px] text-[#ABAEB7]">
+                As development continues, engineers may visit building
+                destinations to guarantee that temporary workers pursue the
+                plan, keep to the timetable, utilize the predefined materials,
+                and meet work-quality models.
+              </p>
+                </div>
+                <div class="flex items-center space-x-4">
+                  <span class="text-6xl font-bold text-white">85%</span>
+                  <p class="text-gray-400 max-w-[78px] w-full">Satisfied Clients</p>
+                </div>
+                <div class="flex gap-[25px] mt-[30px] items-center">
+                  <img src="../public//images/Rectangle 742.png" alt="">
+                  <img src="../public/images/Group 11964.png" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="flex w1/2 gap-[15px]">
+          <img src="../public/images/Rectangle 723.png" alt="">
+          <img src="../public/images/Rectangle 722.png" alt="">
+        </div>
+      </div>
+    </section>
+    <section class="relative">
+      <div class="container mt-[100px]">
+        <p class="font-normal text-[16px] leading-[20px] text-[#FFC978] mb-[15px]">OUR PROJECT</p>
+        <div class="flex items-center justify-between">
+          <p class="font-normal text-[60px] leading-[77px] text-white">Our Latest Projects</p>
+          <ul class="flex gap-[25px]">
+            <li class="font-bold text-[16px] leading-[26px] text-[#FFB076]">All</li>
+            <li class="font-bold text-[16px] leading-[26px] text-white">Architecture</li>
+            <li class="font-bold text-[16px] leading-[26px] text-white">Interior Design</li>
+            <li class="font-bold text-[16px] leading-[26px] text-white">Construction</li>
+          </ul>
+        </div>
+        <div class="grid grid-cols-4 mt-[50px] gap-[20px] absolute">
+          <img src="../public/images/Rectangle 725.png" alt="">
+          <img class="relative top-[100px]" src="../public/images/Rectangle 726.png" alt="">
+          <img src="../public/images/Rectangle 727.png" alt="">
+          <img class="relative top-[100px]" src="../public/images/Rectangle 728.png" alt="">
+          <img src="../public/images/Rectangle 754.png" alt="">
+          <img class="relative top-[100px]" src="../public/images/Rectangle 755.png" alt="">
+          <img src="../public/images/Rectangle 757.png" alt="">
+          <img class="relative top-[100px]" src="../public/images/Rectangle 756.png" alt="">
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container">
+        <img src="../public/images/Group 11770.png" alt="">
+        <div>
+          <p class="font-normal text-[60px] leading-[70px] text-white max-w-[670px] w-full">We Create Digital Experience</p>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="container mt-[1500px]">
+        <p class="font-normal text-[16px] leading-[20px] text-[#FFC978] mb-[20px]">OUR PROJECT</p>
+        <p class="font-normal text-[60px] leading-[70px] text-white mb-[50px]">Meet With Our Team</p>
+        <img src="../public/images/Rectangle 4385.png" alt="">
+        <div>
+          <p class="font-medium text-[24px] leaading-[28px] text-white mb-[10px] mt-[20px]">Cody Fisher   -   CEO</p>
+        </div>
+        <hr class="mb-[10px] text-white">
+        <div>
+          <p class="font-medium text-[24px] leaading-[28px] text-white mb-[10px]">Savannah Nguyen   -   Interior architect</p>
+        </div>
+        <hr class="mb-[10px] text-white">
+        <div>
+          <p class="font-medium text-[24px] leaading-[28px] text-white mb-[10px]">Jerome Bell   -   Interior</p>
+        </div>
+        <hr class="mb-[10px] text-white">
+        <div>
+          <p class="font-medium text-[24px] leaading-[28px] text-white mb-[10px]">Cameron Williamson   -   architect</p>
+        </div>
+        <hr class="mb-[10px] text-white">
+        <div>
+          <p class="font-medium text-[24px] leaading-[28px] text-white mb-[10px]">Marvin McKinney   -   COnstruction head</p>
+        </div>
+        <hr class="mb-[10px] text-white">
+      </div>
+    </section>
+  </main>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        dark: "#0B0E17",
+        gray: "#ABAEB7",
+        primary: "#FFC978",
+      },
+      backgroundImage: {
+        body: "url('/images/background.png')",
+      },
+    },
+  },
+  plugins: [],
+}
+
